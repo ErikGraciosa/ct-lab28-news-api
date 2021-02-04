86d91cc5967e2ddac73c7671b960d72a87addfba
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-04 08:12:11 -0800

    articles fetch working

--- src/components/app/loadingSpinner/Spinner.jsx
+++ src/components/app/loadingSpinner/Spinner.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function LoadingSpinner() {
   return <div>
-            <iframe title="spinner" src="https://giphy.com/embed/52qtwCtj9OLTi" width="480" height="206" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/loading-52qtwCtj9OLTi">via GIPHY</a></p>
+            <iframe title="spinner" src="https://giphy.com/embed/52qtwCtj9OLTi" width="480" height="206" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/loading-52qtwCtj9OLTi">via GIPHY</a></p>
         </div>;
 }
--- src/containers/newsSearch/NewsSearch.jsx
+++ src/containers/newsSearch/NewsSearch.jsx
@@ -5,11 +5,21 @@ import LoadingSpinner from '../../components/app/loadingSpinner/Spinner';
 
 export default class NewsSearch extends Component {
   state = {
-    search: '',
+    search: 'dogs',
     loading: false,
     articles: []
   }
+
+  componentDidMount = async () => {
+    const articles = await fetch(`https://newsapi.org/v2/everything?q=${this.state.search}&apiKey=e30bf2369aec4d03987a662e3c476593`)
+      .then(res => res.json())
+      .then(json => json.articles);
+    console.log(articles)
+  }
+
+
   render() {
+    console.log(this.state)
     return (   
       <> 
         <Search />
