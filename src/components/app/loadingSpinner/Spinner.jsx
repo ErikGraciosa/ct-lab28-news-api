02b4b6491547291092190a4c158ba3aca9a0052c
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-04 07:48:48 -0800

    added a loading component

---
+++ src/components/app/loadingSpinner/Spinner.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LoadingSpinner() {
+  return <div>
+            <iframe title="spinner" src="https://giphy.com/embed/52qtwCtj9OLTi" width="480" height="206" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/loading-52qtwCtj9OLTi">via GIPHY</a></p>
+        </div>;
+}
--- src/containers/newsSearch/NewsSearch.jsx
+++ src/containers/newsSearch/NewsSearch.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Search from '../../components/search/Search';
 import ArticleList from '../../components/articleList/ArticleList';
+import LoadingSpinner from '../../components/app/loadingSpinner/Spinner';
 
 export default class NewsSearch extends Component {
+  state = {
+    search: '',
+    loading: false,
+    articles: []
+  }
   render() {
     return (   
       <> 
         <Search />
         <ArticleList />
+        <LoadingSpinner />
       </>
     );
   }
